eac2ecd1c69a058cb2f0d8e05712f8bafbf7ece5
obaida <obaidashurbaji99@gmail.com>
2022-12-25 14:20:59 +0200

    some fixes

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -11,18 +11,20 @@ Welcome to E-Shop
    <div class="py-5">
       <div class="container">
          <div class="row">
-            <h2>Featured Products</h2>
+            <h2>Trending Products</h2>
             <div class="owl-carousel featured-carousel owl-theme">
                @foreach ($featured_products as $product )
                   <div class="item">
-                     <div class="card">
-                        <img src="{{ asset('assets/uploads/products/' . $product->image) }}" alt="Product Image" class="featured-img">
-                        <div class="card-body">
-                           <h5>{{ $product->name }}</h5>
-                           <span class="float-start">{{ $product->selling_price }}</span>
-                           <span class="float-end"><s>{{ $product->original_price }}</s></span>
+                     <a href="{{ url('category/' . $product->category->slug . '/' . $product->slug) }}">
+                        <div class="card">
+                           <img src="{{ asset('assets/uploads/products/' . $product->image) }}" alt="Product Image" class="featured-img">
+                           <div class="card-body">
+                              <h5>{{ $product->name }}</h5>
+                              <span class="float-start">{{ $product->selling_price }}</span>
+                              <span class="float-end"><s>{{ $product->original_price }}</s></span>
+                           </div>
                         </div>
-                     </div>
+                     </a>
                   </div>
                @endforeach
             </div>
@@ -33,7 +35,7 @@ Welcome to E-Shop
    <div class="py-5">
       <div class="container">
          <div class="row">
-            <h2>Trending Products</h2>
+            <h2>Trending Categories</h2>
             <div class="owl-carousel featured-carousel owl-theme">
             @foreach ($trending_category as $category )
                <div class="item">
