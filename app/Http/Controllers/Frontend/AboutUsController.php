0c261fd537e48110ccda065fe6b4b4bac05fbf1d
obaida <obaidashurbaji99@gmail.com>
2022-12-31 01:17:58 +0200

    aboutus page

--- app/Http/Controllers/Frontend/AboutUsController.php
+++ app/Http/Controllers/Frontend/AboutUsController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Team;
 use Illuminate\Http\Request;
 
 class AboutUsController extends Controller
 {
     public function index() {
-        return view('frontend.about');
+        $team = Team::all();
+        return view('frontend.about', compact('team'));
     }
 }
--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -103,60 +103,18 @@
                     <h2 class="section-title">Our Team</h2>
                 </div>
             </div>
-
-            <div class="row">
-
+            <div class="row d-flex justify-content-around">
                 <!-- Start Column 1 -->
+                @foreach($team as $member)
                 <div class="col-12 col-md-6 col-lg-3 mb-5 mb-md-0">
-                    <img src="{{ asset("frontend/images/person_1.jpg") }}" class="img-fluid mb-5">
-                    <h3><a href="#"><span class="">Lawson</span> Arnold</a></h3>
-                    <span class="d-block position mb-4">CEO, Founder, Atty.</span>
-                    <p>Separated they live in.
-                        Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+                    <img src="{{ asset("assets/uploads/team/" . $member->image) }}" class="img-fluid mb-5">
+                    <h3><a href="#"><span class="">{{ $member->name }}</span></a></h3>
+                    <span class="d-block position mb-4">{{ $member->title }}</span>
+                    <p>{{ $member->description }}</p>
                     <p class="mb-0"><a href="#" class="more dark">Learn More <span class="icon-arrow_forward"></span></a></p>
                 </div>
+                @endforeach
                 <!-- End Column 1 -->
-
-                <!-- Start Column 2 -->
-                <div class="col-12 col-md-6 col-lg-3 mb-5 mb-md-0">
-                    <img src="{{ asset("frontend/images/person_2.jpg") }}" class="img-fluid mb-5">
-
-                    <h3><a href="#"><span class="">Jeremy</span> Walker</a></h3>
-                    <span class="d-block position mb-4">CEO, Founder, Atty.</span>
-                    <p>Separated they live in.
-                        Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
-                    <p class="mb-0"><a href="#" class="more dark">Learn More <span class="icon-arrow_forward"></span></a></p>
-
-                </div>
-                <!-- End Column 2 -->
-
-                <!-- Start Column 3 -->
-                <div class="col-12 col-md-6 col-lg-3 mb-5 mb-md-0">
-                    <img src="{{ asset("frontend/images/person_3.jpg") }}" class="img-fluid mb-5">
-                    <h3><a href="#"><span class="">Patrik</span> White</a></h3>
-                    <span class="d-block position mb-4">CEO, Founder, Atty.</span>
-                    <p>Separated they live in.
-                        Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
-                    <p class="mb-0"><a href="#" class="more dark">Learn More <span class="icon-arrow_forward"></span></a></p>
-                </div>
-                <!-- End Column 3 -->
-
-                <!-- Start Column 4 -->
-                <div class="col-12 col-md-6 col-lg-3 mb-5 mb-md-0">
-                    <img src="{{asset("frontend/images/person_4.jpg")}}" class="img-fluid mb-5">
-
-                    <h3><a href="#"><span class="">Kathryn</span> Ryan</a></h3>
-                    <span class="d-block position mb-4">CEO, Founder, Atty.</span>
-                    <p>Separated they live in.
-                        Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
-                    <p class="mb-0"><a href="#" class="more dark">Learn More <span class="icon-arrow_forward"></span></a></p>
-
-
-                </div>
-                <!-- End Column 4 -->
-
-
-
             </div>
         </div>
     </div>
