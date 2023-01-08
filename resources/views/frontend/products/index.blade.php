b977c0379adc9c2fe5e86e5c082af53799cf9d35
obaida <obaidashurbaji99@gmail.com>
2023-01-08 21:07:13 +0200

    fixes

--- resources/views/frontend/products/index.blade.php
+++ resources/views/frontend/products/index.blade.php
@@ -9,7 +9,7 @@
 <div class="py-3 mb-4 shadow-sm bg-warning border-top">
    <div class="container">
       <h6 class="mb-0">
-         <a href="{{ url('category') }}">Collections</a> / 
+         <a href="{{ url('category') }}">Collections</a> /
          <a href="{{ url('category/' . $category->slug ) }}">{{ $category->name }}</a>
       </h6>
    </div>
@@ -19,7 +19,7 @@
       <div class="container">
          <div class="row">
             <h2>{{ $category->name }}</h2>
-            @foreach ($product as $product )
+            @foreach ($product as $product)
                <div class="col-md-3 mb-3">
                   <div class="card">
                      <a href="{{ url('category/' . $category->slug . '/' . $product->slug) }}">
